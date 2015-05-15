92b29defb7c93e5b6e284fd3e6de19adc756d068
dl-eric <orangepamelo@gmail.com>
2015-05-15 12:39:37 -0400

    Update ZetDeck.java

--- SetGame/ZetDeck.java
+++ SetGame/ZetDeck.java
@@ -1,14 +1,28 @@
-import java.util.ArrayList;
-
+/*
+ * Made by Helen
+ * Edited by Eric
+ */
 
 public class ZetDeck extends Deck
 {
-  public ArrayList<ZetCard> Zetcards;
-
   //Constructs a ZetDeck
   public ZetDeck()
   { 
-    Zetcards = new ArrayList<ZetCard>(81);
+    super(81);
+    
+    for(int shape = 1; shape <= 3; shape++)
+    {
+      for(int fill = 1; fill <= 3; fill++)
+      {
+        for(int number = 1; number <= 3; number++)
+        {
+          for(int color = 1; color <= 3; color++)
+          {
+            ZetCard card = new ZetCard(number, shape, fill, color);
+            add(card);
+          }
+        }
+      }
+    }
   }
 }
-
