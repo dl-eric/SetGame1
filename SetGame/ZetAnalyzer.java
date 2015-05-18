b2ea227974fc349dc2a24d71f9a97e743d640176
dl-eric <orangepamelo@gmail.com>
2015-05-18 10:25:14 -0400

    Update ZetAnalyzer.java

--- SetGame/ZetAnalyzer.java
+++ SetGame/ZetAnalyzer.java
@@ -5,16 +5,17 @@
 
 public class ZetAnalyzer
 {
+  //Pre-condition all cards are different
   public static boolean isZet(ZetCard card1, ZetCard card2, ZetCard card3)
   {
-     if(((card1.getShape() + card2.getShape() + card3.getShape()) % 3 == 0) &&
-         ((card1.getFill() + card2.getFill() + card3.getFill()) % 3 == 0) &&
-         ((card1.getColor() + card2.getColor() + card3.getColor()) % 3 == 0) &&
-         ((card1.getNumber() + card2.getNumber() + card3.getNumber()) % 3 == 0))
-       return true;
-     else
-       return false;
-         
+    if(card1.equals(card2) && card2.equals(card3) && card1.equals(card3))
+      return false;
+    else
+      return(((card1.getShape() + card2.getShape() + card3.getShape()) % 3 == 0) &&
+          ((card1.getFill() + card2.getFill() + card3.getFill()) % 3 == 0) &&
+          ((card1.getColor() + card2.getColor() + card3.getColor()) % 3 == 0) &&
+          ((card1.getNumber() + card2.getNumber() + card3.getNumber()) % 3 == 0));
+
   }
 
   public static int[] findZet(ZetCard[] cards)
@@ -24,7 +25,7 @@ public class ZetAnalyzer
     {
       for(int b = a + 1; b < cards.length; b++)
       {
-        for(int c = b + 1; b < cards.length; c++)
+        for(int c = b + 1; c < cards.length; c++)
         {
           if (isZet(cards[a], cards[b], cards[c]))
           {
